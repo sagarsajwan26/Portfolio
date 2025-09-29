b8574fb57231946ef6652e90f15ecf99ec5b099d
sagarsajwan26 <sagarsajwan26@gmail.com>
2025-09-29 22:36:09 +0530

    m

--- server/server.js
+++ server/server.js
@@ -2,10 +2,8 @@ import { app } from "./src/app.js";
 import { connectDB } from "./src/db/db.js";
 import 'dotenv/config'
 const PORT= process.env.PORT || 3000;
-
-
 connectDB().then(()=>{
     app.listen(PORT,()=>{
-    console.log(`server is running on port ${PORT}`)
+    console.log(`server is running on port : ${PORT}`)
 })
-}) 
+})
