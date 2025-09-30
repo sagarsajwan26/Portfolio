40fabc986d2bae70db97c12e2476358d809075fd
sagarsajwan26 <sagarsajwan26@gmail.com>
2025-09-30 21:57:49 +0530

    f

--- server/src/app.js
+++ server/src/app.js
@@ -16,7 +16,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 app.use(cors({
-    origin:["http://localhost:5173", "http://localhost:3000"],
+    origin:["http://localhost:5173", "https://vercel.com/sagarsajwan26s-projects/portfolio-iy2i/BZdG5vCTVt4yV3FvRPNkwY6Jd8o6"],
     credentials:true,
     methods:["GET","POST","PUT","DELETE"]
 }))
